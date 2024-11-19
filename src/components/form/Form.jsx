68d15e39b78def1fdf3b432d18fe05242ca61e95
JacopoTrincanato <trincanatoj@gmail.com>
2024-11-19 17:32:14 +0100

    fix bugs

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -21,6 +21,7 @@ const addedPost = {
     title: "",
     image: "",
     content: "",
+    category: "published",
     tags: [],
     published: true
 }
@@ -29,7 +30,7 @@ const addedPost = {
 export default function Form() {
     const [formData, setFormData] = useState(addedPost)
     const [initialPosts, setInitialPosts] = useState(posts)
-    const [newPosts, setNewPosts] = useState('')
+    {/*const [newPosts, setNewPosts] = useState('')*/ }
 
     //creo una funzione per aggiungere un titolo
     function addPost(e) {
@@ -41,7 +42,6 @@ export default function Form() {
         }
 
         setInitialPosts([newItem, ...initialPosts])
-        setNewPosts('')
 
         setFormData(addedPost)
 
@@ -53,7 +53,7 @@ export default function Form() {
 
         setFormData({
             ...formData,
-            [e.target.title]: value
+            [e.target.name]: value
         })
     }
 
@@ -75,22 +75,61 @@ export default function Form() {
 
             <form onSubmit={addPost}>
 
-                <input type="text" placeholder="Inserisci il titolo" className={style.placeholder} value={newPosts} onChange={e => setNewPosts(e.target.value)} />
+                <input type="text"
+                    placeholder="Inserisci il titolo"
+                    className={style.placeholder}
+                    name="title"
+                    required
+                    value={formData.title}
+                    onChange={handleFormField}
+                />
 
-                <input type="text" placeholder="Inserisci l'immagine" className={style.placeholder} name="image" value={formData.image} onChange={handleFormField} />
+                <input type="text"
+                    placeholder="Inserisci l'immagine"
+                    className={style.placeholder}
+                    name="image"
+                    value={formData.image}
+                    onChange={handleFormField} />
 
-                <textarea name="content" placeholder="Inserisci il contenuto" id="content" rows="5" className={style.placeholder} value={formData.content} onChange={handleFormField}></textarea>
+                <textarea name="content"
+                    placeholder="Inserisci il contenuto"
+                    id="content" rows="5"
+                    className={style.placeholder}
+                    value={formData.content}
+                    onChange={handleFormField}>
 
-                <select name="category" id="category" className={style.placeholder} value={formData.published} onChange={handleFormField}>
+                </textarea>
+
+                <select name="category"
+                    id="category"
+                    className={style.placeholder}
+                    value={formData.published ? "published" : "notPublished"}
+                    onChange={handleFormField}>
                     <option value="published">Published</option>
                     <option value="notPublished">not Published</option>
                 </select>
 
                 <div>
-                    <input type="checkbox" value={formData.tags} onChange={handleFormField} />html
-                    <input type="checkbox" value={formData.tags} onChange={handleFormField} />css
-                    <input type="checkbox" value={formData.tags} onChange={handleFormField} />js
-                    <input type="checkbox" value={formData.tags} onChange={handleFormField} />php
+                    <input type="checkbox"
+                        value={formData.tags.includes('html')}
+                        onChange={handleFormField}
+                    />html
+
+                    <input type="checkbox"
+                        value={formData.tags.includes('css')}
+                        onChange={handleFormField}
+                    />css
+
+                    <input type="checkbox"
+                        value={formData.tags.includes('js')}
+                        onChange={handleFormField}
+                    />js
+
+                    <input type="checkbox"
+                        value={formData.tags.includes('php')}
+                        onChange={handleFormField}
+                    />php
+
                 </div>
 
 
@@ -100,7 +139,7 @@ export default function Form() {
 
             </form>
 
-            {posts.map((post, index) => <Card key={post.id} data={post}><button onClick={eliminate} data-index={index} className={style.deleteBtn}><FontAwesomeIcon icon={faTrash} /></button></Card>)}
+            {initialPosts.map((post, index) => <Card key={post.id} post={post}><button onClick={eliminate} data-index={index} className={style.deleteBtn}><FontAwesomeIcon icon={faTrash} /></button></Card>)}
 
             {/*< ul >
                 <li><h2>Titoli dei post</h2></li>
